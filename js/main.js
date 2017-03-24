3468c035078e292f378d0b438b9ec853fce26858
joannjiang <qiongrongjiang@gmail.com>
2017-03-24 11:09:34 -0400

    add lazy loading for jsbin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage();
-});
+  $('iframe').each(function(i, obj){
+    var src = $(obj).attr('data-url')
+    $(obj).attr('data-src', src)
+  })
+
+  $('#fullpage').fullpage({
+    lazyLoading: true,
+    navigation: true,
+    scrollingSpeed: 1000
+  })
+})
